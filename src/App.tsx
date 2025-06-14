3f43beca55e59eab28380596f25a556358df9535
greatseun02 <goodnews.adewole9@gmail.com>
2025-06-14 14:34:19 +0100

    Setup and installed react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Dashboard from './Layout/Dashboard'
+import Auth from './Layout/Auth'
+import Home from './Pages/Home'
+import ViewCourses from './Pages/ViewCourses'
+import Login from './Pages/Login'
+import Signup from './Pages/Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard/>}>
+          <Route index element={<Home/>}/>
+          <Route path='view-courses' element={<ViewCourses/>}/>
+          <Route path='view-courses/:username' element={<ViewCourses/>}/>
+          <Route path='view-course' element={<></>} />
+        </Route>
+        <Route path='/' element={<Auth/>}>
+          <Route path='login' element={<Login/>}/>
+          <Route path='signup' element={<Signup/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Pages/ViewCourses.tsx
@@ -0,0 +1,6 @@
+
+export default function ViewCourses() {
+  return (
+    <div>ViewCourses</div>
+  )
+}
